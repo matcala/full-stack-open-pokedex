6de29fee1924351fd4fb4ecd19a84d48690375ee
Matteo Calabrese <matteo.calabrese1@gmail.com>
2022-07-27 01:50:31 +0300

    Complete exercise 11.11

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 9000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log('server started on port 9000')
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
+
+app.get('/version', (req, res) => {
+  res.send('1.0.0') // change this string to ensure a new version deployed
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
+
