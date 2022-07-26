df99a0e59bac7e99472c94e02b41a11f23681ec1
Matteo Calabrese <matteo.calabrese1@gmail.com>
2022-07-27 00:54:57 +0300

    Fix cypress testing

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     video: false,
-    pageLoadTimeout: 10000
+    pageLoadTimeout: 100000
   },
 
   component: {
